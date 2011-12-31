7ea25926785cf9621aa25384621823721ce85d38
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-12-31 20:24:25 +0900

    fix helper

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'fluent-plugin-reducer'
+require 'fluent/plugin/out_exec_reducer'
 
 class Test::Unit::TestCase
 end
