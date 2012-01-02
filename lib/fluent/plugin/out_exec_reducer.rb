5f36dfd38ed16c7a6cf19edf6ab018712554914b
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-01-02 17:40:27 +0900

    implement and wrote tests

--- lib/fluent/plugin/out_exec_reducer.rb
+++ lib/fluent/plugin/out_exec_reducer.rb
@@ -2,39 +2,219 @@ class Fluent::ExecReducerOutput < Fluent::TimeSlicedOutput
   Fluent::Plugin.register_output('exec_reducer', self)
 
   config_set_default :buffer_type, 'memory'
-  config_set_default :time_slice_format, '%Y%m%d' # %Y%m%d%H
+  config_set_default :time_slice_format, '%Y%m%d%H' # %Y%m%d%H%M
 
-  # config_param :hoge, :string, :default => 'hoge'
+  config_param :command, :string
+  config_param :in_keys, :string
+  config_param :remove_prefix, :string, :default => nil
+  config_param :out_keys, :string
+  config_param :strip_output, :bool, :default => true
+  config_param :add_prefix, :string, :default => nil
+  config_param :tag, :string, :default => nil
+  config_param :tag_key, :string, :default => nil
+  config_param :time_key, :string, :default => nil
+  config_param :time_format, :string, :default => nil
+  config_param :localtime, :bool, :default => true
 
   def initialize
     super
-    # require 'hogepos'
+    require 'time'
   end
 
   def configure(conf)
     super
-    # @path = conf['path']
+
+    @localtime = if conf.has_key?('localtime')
+                   conf['localtime']
+                   true
+                 else
+                   false
+                 end
+
+    if !@tag && !@tag_key
+      raise ConfigError, "'tag' or 'tag_key' option is required on exec_reducer output"
+    end
+
+    @in_keys = @in_keys.split(',')
+    @out_keys = @out_keys.split(',')
+
+    if @time_key
+      if @time_format
+        @timef = Fluent::TimeFormatter.new(@time_format, @localtime)
+        @time_format_proc = @timef.method(:format)
+        @time_parse_proc = Proc.new {|str| Time.strptime(str, @time_format).to_i }
+      else
+        @time_format_proc = Proc.new {|time| time.to_s }
+        @time_parse_proc = Proc.new {|str| str.to_i }
+      end
+    end
+
+    if @remove_prefix
+      @removed_prefix_string = @remove_prefix + '.'
+      @removed_length = @removed_prefix_string.length
+    end
+    if @add_prefix
+      @added_prefix_string = @add_prefix + '.'
+    end
+
+    @procs = {} # time_slice_key => {:pid => pid, :thread => thread, :io => popen-object}
   end
 
   def start
     super
-    # init
+    $log.debug "out_exec_reducer start called!!!!"
+    start_watch
+  end
+
+  def start_watch
+    # for internal, or tests only
+    @watcher = Thread.new(&method(:watch))
+  end
+
+  def watch
+    while true
+      sleep 0.5
+      now_slice = @time_slicer.call(Engine.now.to_i - @time_slice_wait)
+      $log.debug "watching slices, from now_slice #{now_slice}"
+      slices = @procs.keys
+      slices.each do |target_slice|
+        next if target_slice >= now_slice
+        $log.debug "closing expired slice #{target_slice}"
+        proc = @procs[target_slice]
+        proc[:io].close_write if proc[:io]
+        proc[:thread].join #TODO set timeout
+        proc.delete(:io)
+        proc.delete(:thread)
+        $log.debug "closed slice #{target_slice}"
+        @procs.delete(target_slice)
+      end
+    end
   end
 
   def shutdown
     super
-    # destroy
+
+    $log.debug "out_exec_reducer shutdown called!!!!"
+    return nil if @procs.keys.length < 1
+    
+    slices = @procs.keys
+    slices.each do |s|
+      if @procs[s][:io]
+        io = @procs[s].delete(:io)
+        io.close_write
+      end
+    end
+    slices.each do |s|
+      @procs[s][:thread].join(1)
+    end
+
+    @watcher.terminate if @watcher.alive?
+
+    slices.each do |s|
+      @procs[s][:thread].terminate
+      @procs[s][:thread].join
+    end
+    nil
   end
 
   def format(tag, time, record)
-    [tag, time, record].to_msgpack
+    if @remove_prefix
+      if tag == @remove_prefix or (tag[0,@removed_length] == @removed_prefix_string and tag.length > @removed_length)
+        tag = tag[@removed_length..-1] || ''
+      end
+    end
+    @in_keys.map{|key|
+      case key
+      when @time_key
+        @time_format_proc.call(time)
+      when @tag_key
+        tag
+      else
+        record[key].to_s
+      end
+    }.join("\t") + "\n"
+  end
+
+  def run(io, pid, slice_time)
+    while not io.eof?
+      begin
+        line = io.readline
+        line.chomp!
+        vals = line.split("\t")
+        tag = @tag
+        time = nil
+        record = {}
+        for i in 0...(@out_keys.length)
+          key = @out_keys[i]
+          val = vals[i]
+          if key == @time_key
+            time = @time_parse_proc.call(val)
+          elsif key == @tag_key
+            tag = if @add_prefix
+                    @added_prefix_string + val
+                  else
+                    val
+                  end
+          else
+            if @strip_output
+              record[key] = val.strip
+            else
+              record[key] = val
+            end
+          end
+        end
+        if tag
+          time ||= slice_time
+          Fluent::Engine.emit(tag, time, record)
+        end
+      rescue
+        $log.error "exec_reducer failed to emit", :error => $!, :line => line
+        $log.error_backtrace $!.backtrace
+      end
+    end
+    Process.waitpid(pid) rescue true # ignore 'No child process' and others
+  rescue
+    $log.error "bailouting"
+    Process.kill(:TERM, pid) rescue true # ignore errors
+    Process.kill(:KILL, pid) rescue true # ignore errors
+    $log.error "process killed...."
+    raise
+  end
+
+  def wait_thread(slice)
+    # for test only
+    $log.debug "waiting for slice #{slice}"
+    proc = @procs[slice]
+    $log.debug "process for slice #{slice}: " + proc.inspect
+    if proc and proc[:thread]
+      thread = proc[:thread]
+      $log.debug "status for waiting thread #{thread.object_id}: #{thread.status}"
+      thread.join
+    end
   end
 
   def write(chunk)
-    records = []
-    chunk.msgpack_each { |record|
-      # records << record
-    }
-    # write records
+    unless @procs[chunk.key]
+      _io = IO.popen(@command, 'r+')
+      _io.sync = true
+      _pid = _io.pid
+      slice_time = Time.strptime(chunk.key, @time_slice_format).to_i
+      _thread = Thread.new(_io, _pid, slice_time, &method(:run))
+      @procs[chunk.key] = {:thread => _thread, :io => _io}
+    end
+    io = @procs[chunk.key][:io]
+    now_slice = @time_slicer.call(Fluent::Engine.now.to_i - @time_slice_wait).to_i
+    begin
+      io.write chunk.read
+      if chunk.key.to_i < now_slice
+        $log.debug "closing fd in write for expired time slice"
+        io.close_write
+        @procs[chunk.key].delete(:io)
+        @procs[chunk.key][:thread].join #TODO set timeout
+      end
+    rescue
+      $log.error "failed to write chunk"
+      raise
+    end
   end
 end
--- test/plugin/test_out_exec_reducer.rb
+++ test/plugin/test_out_exec_reducer.rb
@@ -9,56 +9,130 @@ class ExecReducerOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    command wc -l
+    in_keys time,k1
+    out_keys k2
+    time_key time
+    tag reduced
+    time_slice_wait 1s
+    flush_interval 1s
   ]
-  # CONFIG = %[
-  #   path #{TMP_DIR}/out_file_test
-  #   compress gz
-  #   utc
-  # ]
 
-  def create_driver(conf = CONFIG)
-    Fluent::Test::TimeSlicedOutputTestDriver.new(Fluent::ExecReducerOutput).configure(conf)
+  def create_driver(conf=CONFIG, tag='test')
+    Fluent::Test::TimeSlicedOutputTestDriver.new(Fluent::ExecReducerOutput, tag).configure(conf)
   end
 
   def test_configure
-    #### set configurations
-    # d = create_driver %[
-    #   path test_path
-    #   compress gz
-    # ]
-    #### check configurations
-    # assert_equal 'test_path', d.instance.path
-    # assert_equal :gz, d.instance.compress
+    d = create_driver
+
+    assert_equal '%Y%m%d%H', d.instance.time_slice_format
+    assert_equal ['time', 'k1'], d.instance.in_keys
+    assert_equal ['k2'], d.instance.out_keys
+    assert_equal 'time', d.instance.time_key
+    assert_equal "reduced", d.instance.tag
+    assert_equal false, d.instance.localtime
+    assert_equal true, d.instance.strip_output
+
+    d = create_driver %[
+      time_slice_format %Y%m%d%H%M
+      command sort -u
+      in_keys time,tag,k1
+      remove_prefix before
+      out_keys time,tag,k2
+      add_prefix after
+      tag_key tag
+      time_key time
+      time_format %Y%m%d%H%M
+      localtime
+    ]
+
+    assert_equal '%Y%m%d%H%M', d.instance.time_slice_format
+    assert_equal 'sort -u', d.instance.command
+    assert_equal ['time', 'tag', 'k1'], d.instance.in_keys
+    assert_equal 'before', d.instance.remove_prefix
+    assert_equal ['time', 'tag', 'k2'], d.instance.out_keys
+    assert_equal 'after', d.instance.add_prefix
+    assert_equal 'tag', d.instance.tag_key
+    assert_equal 'time', d.instance.time_key
+    assert_equal '%Y%m%d%H%M', d.instance.time_format
+    assert_equal true, d.instance.localtime
   end
 
   def test_format
     d = create_driver
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    d.emit({"k1"=>'number one'}, time)
+    d.emit({"k1"=>'number two'}, time)
+    d.expect_format time.to_s + %[\tnumber one\n]
+    d.expect_format time.to_s + %[\tnumber two\n]
+    # d.instance.start_watch
+    d.run
+    d.instance.wait_thread('2011010222')
 
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n]
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n]
-
-    # d.run
+    d = create_driver(%[
+      time_slice_format %Y%m%d%H%M
+      command sort -u
+      in_keys time,tag,k1
+      remove_prefix before
+      out_keys time,tag,k2
+      add_prefix after
+      tag_key tag
+      time_key time
+      time_format %Y%m%d%H%M
+      localtime
+    ], 'before.testXXX')
+    time = Time.parse("2011-01-02 13:14:15 JST").to_i
+    d.emit({"k1"=>'value one'}, time)
+    d.emit({"k1"=>'value two'}, time)
+    d.expect_format %[201101021314\ttestXXX\tvalue one\n]
+    d.expect_format %[201101021314\ttestXXX\tvalue two\n]
+    # d.instance.start_watch
+    d.run
+    d.instance.wait_thread('201101021314')
   end
 
   def test_write
     d = create_driver
+    time1 = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    timeslice_1 = Time.parse("2011-01-02 13:00:00 UTC").to_i
+    d.emit({"k1"=>11}, time1)
+    d.emit({"k1"=>21}, time1)
+    d.emit({"k1"=>31}, time1)
+    d.emit({"k1"=>41}, time1)
+    d.run
+    d.instance.wait_thread('2011010222')
+    emits = d.emits
+    assert_equal 1, emits.length
+    assert_equal ['reduced', timeslice_1, {"k2"=>'4'}], emits[0]
 
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
+    d = create_driver
+    time1 = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    timeslice_1 = Time.parse("2011-01-02 13:00:00 UTC").to_i
+    time2 = Time.parse("2011-01-02 14:15:16 UTC").to_i
+    timeslice_2 = Time.parse("2011-01-02 14:00:00 UTC").to_i
+    d.emit({"k1"=>31}, time1)
+    d.emit({"k1"=>41}, time2)
+    d.run
+    d.instance.wait_thread('2011010222')
+    d.instance.wait_thread('2011010223')
+    emits = d.emits
+    assert_equal 2, emits.length
+    assert_equal ['reduced', timeslice_1, {"k2"=>'1'}], emits[0]
+    assert_equal ['reduced', timeslice_2, {"k2"=>'1'}], emits[1]
+  end
 
-    # ### FileOutput#write returns path
-    # path = d.run
-    # expect_path = "#{TMP_DIR}/out_file_test._0.log.gz"
-    # assert_equal expect_path, path
-
-    # data = Zlib::GzipReader.open(expect_path) {|f| f.read }
-    # assert_equal %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n] +
-    #                 %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n],
-    #              data
+  def test_write_ontime
+    d = create_driver
+    now = Time.now
+    time = now.to_i
+    str = 'fooooooo baaaaarrrrrr'
+    d.emit({"k1"=>str}, time)
+    d.run
+    slice = now.strftime(d.instance.time_slice_format)
+    d.instance.wait_thread(slice)
+    emits = d.emits
+    assert_equal slice, Time.now.strftime(d.instance.time_slice_format)
+    assert_equal 1, emits.length
+    assert_equal ['reduced', Time.strptime(slice, d.instance.time_slice_format).to_i, {"k2"=>'1'}], emits[0]
   end
 end
